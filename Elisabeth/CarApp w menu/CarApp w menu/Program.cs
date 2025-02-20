22e57819c7f335c82553e96ad710b088fa84ef4c
elisabethleonorachristine <elisabethleonorachristine@gmail.com>
2025-02-20 10:41:02 +0100

    YES

---
+++ Elisabeth/CarApp w menu/CarApp w menu/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CarApp_w_menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int Add(int a, int b)
+            {
+                return a + b;
+                
+            }
+            int x = 5;
+            int y = 10;
+            Console.WriteLine(Add(x, y));
+        }
+    }
+}
