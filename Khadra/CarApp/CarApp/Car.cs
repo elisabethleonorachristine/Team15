c51d0b04c4a2e7beff13e1161ce5a0e879b29174
Khadra Khalif <kamk71385@edu.ucl.dk>
2025-03-06 14:09:01 +0100

    Khadras kode

---
+++ Khadra/CarApp/CarApp/Car.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace CarApp
+{
+    internal class Car
+    {
+        private string brand;
+        private string model;
+        private string brændstofstype;
+        private char geartype;
+        private double kmPerLiter;
+        private int year;
+        private int kmStand;
+        private bool isEngineOn = false;
+
+        public Car(string brand, string model, string brændstofstype, char geartype, double kmPerLiter, int year, int kmStand, bool isEngineOn)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.brændstofstype = brændstofstype;
+            this.geartype = geartype;
+            this.kmPerLiter = kmPerLiter;
+            this.year = year;
+            this.kmStand = kmStand;
+            this.isEngineOn = false;
+        }
+
+        public string getBrand() { return brand; }
+        public void setBrand(string brand) { this.brand = brand; }
+        
+        public string getModel() { return model; }
+        public void setModel(string model) { this.model = model; }
+        
+        public string getBrændstofstype() { return brændstofstype; }
+        public void setBrændstofstype(string brændstofstype) { this.brændstofstype = brændstofstype; }
+        
+        public char getGeartype() { return geartype; }
+        public void setGeartype(char geartype) {  this.geartype = geartype; }
+
+        public double getkmPerLiter() { return kmPerLiter; }
+        public void setkmPerLiter(double kmPerliter) { this.kmPerLiter=kmPerliter; }
+
+        public int getYear() { return year; }
+        public void setYear(int year) { this.year = year; }  
+
+        public int getKMStand() { return kmStand; }
+        public void setKMStand(int kmStand) {  this.kmStand = kmStand; }
+       
+        public bool getisEngineOn() { return isEngineOn; }
+        public void setIsEnginOn(bool isEngineOn) { this.isEngineOn = isEngineOn; }
+
+
+        public void StartEngine()
+        {
+            isEngineOn = true;
+            Console.WriteLine("Motoren er tændt.");
+        }
+        public void Drive(double distance)
+        {
+            if (isEngineOn)
+            {
+                kmStand += (int)distance;
+                Console.WriteLine("Bilen har kørt " + distance + " km og den har nu en km stand på " + kmStand);
+            }
+            else
+            {
+                Console.WriteLine("Motoren er ikke tændt, derfor kan bilen ikke køre");
+            }
+        }
+        public double CalculateTripPrice(double distance)
+        {
+
+            //// Brændstofpriser
+            double fuelPrizeB = 13.49;
+            double fuelPrizeD = 12.29;
+            double tripCost;
+
+
+            if (kmPerLiter == 0)
+            {
+                Console.WriteLine("Ugyldigt input:");
+                return 0;
+            }
+
+
+            //// Beregn prisen baseret på brændstoftypen
+            double fuelNeeded = distance / kmPerLiter;
+            if (brændstofstype.ToLower() == "benzin")
+            {
+                tripCost = fuelNeeded * fuelPrizeB;
+
+            }
+            else if (brændstofstype.ToLower() == "diesel")
+            {
+                tripCost = fuelNeeded * fuelPrizeD;
+
+            }
+            else
+            {
+                Console.WriteLine("Ugyldig brændstoftype.Prisen kan ikke beregnes");
+                return 0;
+            }
+            return tripCost;
+        }
+        public void PrintCarDetails()
+        {
+            Console.WriteLine("Bilens mærke: " + brand);
+            Console.WriteLine("Bilens model: " + model);
+            Console.WriteLine("Bilens årgang: " + year);
+            Console.WriteLine("Bilens geartype: " + geartype);
+            Console.WriteLine("Bilens brændstoftype: " + brændstofstype);
+            Console.WriteLine("Denne bil kan køre " + kmPerLiter + " km pr. liter");
+            Console.WriteLine("Bilens nuværende km stand: " + kmStand);
+            Console.WriteLine("Hvor langt vil du have bilen skal køre?");
+            double distance = Convert.ToDouble(Console.ReadLine());
+            double tripPrice = CalculateTripPrice(distance);
+            if (distance > 0)
+            {
+                Console.WriteLine("Prisen for at køre " + distance + " km er " + tripPrice + "kr.");
+
+            }
+            else
+            {
+                Console.WriteLine("Prisen kan ikke beregnes");
+            }
+
+            bool isPalindrome = Program.IsPalindrome(kmStand);
+            if (isPalindrome)
+            {
+                Console.WriteLine("Bilens km. stand er et palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Bilens km stand er ikke et palindrome");
+            }
+        }
+    }
+}
---
+++ Khadra/CarApp/CarApp/Program.cs
@@ -0,0 +1,120 @@
+﻿using System.ComponentModel.Design;
+using CarApp;
+
+internal class Program
+{
+    static string brand;
+    static string model;
+    static string brændstofstype;
+    static char geartype;
+    static double kmPerLiter;
+    static int year;
+    static int kmStand; // a.k.a odomoter gad ikke til at ændre det :P 
+    static bool isEngineOn = false;
+
+
+    private static void Main(string[] args)
+    {
+        bool start = true;
+        Car myCar = null;
+
+        while (start)
+        {
+            Console.WriteLine("\n--- MENU ---");
+            Console.WriteLine("1. Opret en ny bil");
+            Console.WriteLine("2. Vis bilens oplysninger");
+            Console.WriteLine("3. Kør en tur");
+            Console.WriteLine("4. Afslut program");
+
+            string valg = Console.ReadLine();
+            switch (valg)
+            {
+                case "1":
+                    //Jeg anvender konstruktøren og opretter en bil
+                    myCar = new Car("Toyota", "Corolla", "Benzin", 'm', 12.0, 2001,1000, true);
+                    Console.WriteLine("En ny bil er blevet oprettet!");
+                    break;
+                case "2":
+                    if (myCar != null)
+                    {
+                        myCar.PrintCarDetails();
+                    }
+                    else {
+                        Console.WriteLine("Der er ikke oprettet nogen bil");
+                    } 
+                    break;
+                case "3":
+                    if (myCar != null)
+                    {
+                        StartDriving(myCar);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du har ikke oprettet en bil. Opret en bil først");
+                    }
+                    break;
+                case "4":
+                    start = false;
+                    Console.WriteLine("Afslut program");
+                    break;
+
+                default:
+                    Console.WriteLine("Ugyldigt valg. Prøv igen.");
+                    break;
+            }
+        }
+    }
+
+    private static void StartDriving(Car myCar)
+    {
+        Console.WriteLine("\nVil du køre en tur?");
+        Console.WriteLine("1. Ja");
+        Console.WriteLine("2. Nej");
+        string kørValg = Console.ReadLine();
+        if (kørValg == "1")
+        {
+            if (!myCar.getisEngineOn()) {
+                myCar.StartEngine();
+            }
+            Console.WriteLine("Hvor langt vil du køre?");
+            double distance = Convert.ToDouble(Console.ReadLine());
+            myCar.Drive(distance);
+            
+        }
+        else
+        {
+            Console.WriteLine("Det er vel også bedre for miljøet :P");
+        }
+    }
+    public static bool IsPalindrome(int km)
+    {
+        string kmString = km.ToString();
+        int length = kmString.Length;
+
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (kmString[i] != kmString[length - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
