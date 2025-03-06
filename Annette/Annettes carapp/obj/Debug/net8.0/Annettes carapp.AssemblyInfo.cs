bfc51c96d03d7a62db30a0ba84b94ce5475ce9c9
Annette Jensen <annetteduedal@gmail.com>
2025-03-06 20:18:32 +0100

    Opdateret med Car class

--- Annette/Annettes carapp/obj/Debug/net8.0/Annettes carapp.AssemblyInfo.cs
+++ Annette/Annettes carapp/obj/Debug/net8.0/Annettes carapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Annettes carapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11d75451fbdb79a0d2d277d3584d920d31583e77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Annettes carapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Annettes carapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
